1a797282655031e56630cd0a08b6661c2f32c1a5
masonutt99 <65191595+masonutt99@users.noreply.github.com>
2020-05-11 13:51:20 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+/** main.c
+* ===========================================================
+* Name: Mason Utt, 11 May  2020
+* Section: M1
+* Project: Partner Pex
+* ===========================================================
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    printf("Hello World!");
+    return 0;
+};
